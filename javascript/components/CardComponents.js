385e76754f3fce6aad40cd9ac65063794e7d9d30
Felipe S <rfelipe273@gmail.com>
2021-12-05 19:06:30 -0300

    more card stylizations

--- javascript/components/CardComponents.js
+++ javascript/components/CardComponents.js
@@ -18,7 +18,7 @@ class ProjectCard {
         card.classList.add('project-card');
 
         const project_title = document.createElement('h3');
-        project_title.classList.add('project-title');
+        project_title.classList.add('project-card--title');
 
         const project_title_a = document.createElement('a');
         project_title_a.href = this.project.link;
@@ -27,11 +27,11 @@ class ProjectCard {
         project_title.appendChild(project_title_a);
 
         const project_description = document.createElement('p');
-        project_description.classList.add('project-description');
+        project_description.classList.add('project-card--description');
         project_description.textContent = this.project.description;
 
         const project_language = document.createElement('p');
-        project_language.classList.add('project-language');
+        project_language.classList.add('project-card--language');
         project_language.textContent = this.project.language;
 
         card.appendChild(project_title);
