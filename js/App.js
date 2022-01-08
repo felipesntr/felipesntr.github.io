0e8d88918862e7107e2cb02e595d255d98de9e79
Felipe Santos <rfelipe273@gmail.com>
2022-01-07 23:57:11 -0300

    prettier

--- js/App.js
+++ js/App.js
@@ -1,24 +1,25 @@
-const projects_element = document.querySelector('.projects--container');
+const projects_element = document.querySelector(".projects--container");
 
 const repos = () => {
-    const url = `https://api.github.com/users/felipesntr/repos`;
-    return fetch(url)
-        .then(res => res.json())
-        .catch(err => console.log(err));
-}
+  const url = `https://api.github.com/users/felipesntr/repos`;
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-    const repositories = repos();
-    repositories.then(data => data.filter(repository => {
-            const project = new Project(
-                repository.name,
-                repository.description,
-                repository.html_url,
-                repository.language
-            );
-            const project_card = new ProjectCard(project);
-            const project_element = project_card.create();
-            projects_element.appendChild(project_element);
-        }
-    }))
+document.addEventListener("DOMContentLoaded", () => {
+  const repositories = repos();
+  repositories.then((data) =>
+    data.filter((repository) => {
+      const project = new Project(
+        repository.name,
+        repository.description,
+        repository.html_url,
+        repository.language
+      );
+      const project_card = new ProjectCard(project);
+      const project_element = project_card.create();
+      projects_element.appendChild(project_element);
+    })
+  );
 });
